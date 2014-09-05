7ceb3ae09189004959c7265afdc732fe2ad44be2
Nate Dillon <natedillon@gmail.com>
2014-09-04 21:03:54 -0500

    grid layout and spacing fixes

--- sites/all/themes/newhopeks/templates/page.tpl.php
+++ sites/all/themes/newhopeks/templates/page.tpl.php
@@ -1,31 +1,33 @@
 <header role="banner" class="header">
     <div class="container">
-        <div class="row">
-    		<div class="logo col-xs-12 col-sm-3">
-    			<h1>
-    				<?php if (!$is_front) { echo '<a href="/">'; } ?>
-    				    <img src="/<?= path_to_theme(); ?>/img/logo@2x.png" width="215" alt="<?php print $site_name; ?>" />
-    				<?php if (!$is_front) { echo '</a>'; } ?>
-    			</h1>
-    		</div> <!-- end .logo -->
+        <div class="wrapper">
+            <div class="row">
+        		<div class="logo col-xs-12 col-sm-3">
+        			<h1>
+        				<?php if (!$is_front) { echo '<a href="/">'; } ?>
+        				    <img src="/<?= path_to_theme(); ?>/img/logo@2x.png" width="215" alt="<?php print $site_name; ?>" />
+        				<?php if (!$is_front) { echo '</a>'; } ?>
+        			</h1>
+        		</div> <!-- end .logo -->
 
-            <nav role="navigation" class="navbar navbar-main col-xs-12 col-sm-9">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-    	        <div class="collapse navbar-collapse" id="main-navbar-collapse">
-    	            <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('class' => 'nav navbar-nav main-menu'))); ?>
-                    <?php // print $search_form; ?>
-    				<?php if ($secondary_menu) : ?>
-                        <?php print theme('links__menu_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('class' => 'nav navbar-nav secondary-menu'))); ?>
-    				<?php endif; ?>
-    	        </div>
-            </nav>
-        </div>
-    </div>
+                <nav role="navigation" class="navbar navbar-main col-xs-12 col-sm-9">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+        	        <div class="collapse navbar-collapse" id="main-navbar-collapse">
+        	            <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('class' => 'nav navbar-nav main-menu'))); ?>
+                        <?php // print $search_form; ?>
+        				<?php if ($secondary_menu) : ?>
+                            <?php print theme('links__menu_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('class' => 'nav navbar-nav secondary-menu'))); ?>
+        				<?php endif; ?>
+        	        </div>
+                </nav>
+            </div> <!-- end .row -->
+        </div> <!-- end .wrapper -->
+    </div> <!-- end .container -->
 </header> <!-- end .header -->
 
 <?php if ($page['content_above']) : ?>
@@ -42,12 +44,14 @@
 	<div class="container">
 		<div class="row">
 			<div role="main" class="main col-xs-12 <?php if ($is_front || !$page['content_sidebar']) : ?>col-sm-12<?php else : ?>col-sm-9 col-sm-push-3<?php endif; ?>">
-				<?php if (!$is_front && $title) : ?>
-					<h1><?php print $title; ?></h1>
-				<?php endif; ?>
-				<?php $tabs && print render($tabs); ?>
-				<?php if ($is_front) { unset($page['content']['system_main']); } ?>
-				<?php print render($page['content']); ?>
+			    <div class="wrapper">
+    				<?php if (!$is_front && $title) : ?>
+    					<h1><?php print $title; ?></h1>
+    				<?php endif; ?>
+    				<?php $tabs && print render($tabs); ?>
+    				<?php if ($is_front) { unset($page['content']['system_main']); } ?>
+    				<?php print render($page['content']); ?>
+			    </div> <!-- end .wrapper -->
 			</div> <!-- end .main -->
 
 			<?php if (!$is_front && $page['content_sidebar']) : ?>
