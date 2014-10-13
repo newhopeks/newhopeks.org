40d22c0a08e199492132f78a101755157bd521c6
Nate Dillon <natedillon@gmail.com>
2014-10-13 17:08:35 -0500

    content block refactor
    Reconfiguring the content blocks for messages, news, and events for use in main content area as well as the sidebar and home page.

---
+++ sites/all/themes/newhopeks/templates/ds-reset--node-message-teaser.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Display Suite reset template.
+ */
+?>
+<div class="article-listing__item">
+    <?php print $ds_content; ?>
+    <?php if (!empty($drupal_render_children)): ?>
+      <?php print $drupal_render_children ?>
+    <?php endif; ?>
+</div> <!-- end .article-listing__item -->
