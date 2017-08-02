d83499b1f4270e5550236222ed307b64e1c2fd94
Nate Dillon <natedillon@gmail.com>
2017-08-02 16:39:59 -0500

    Added small screen nav toggle

--- sites/all/themes/newhopeks/js/script.js
+++ sites/all/themes/newhopeks/js/script.js
@@ -13,6 +13,34 @@ Copyright:  (c) 2014 New Hope Church
 
 (function ($) { $(document).ready(function() {
 
+  // Navigation
+  // -------------------------
+
+  // Add the navigation toggle button
+  $('.header .nav').before('<button type="button" class="nav-toggle"><i class="fa fa-bars"></i> Menu</button>');
+
+  // Prepare the navigation
+  $('.header .nav').attr('data-nav-status', 'closed');
+
+  // Toggle the navigation
+  $('.header .nav-toggle').on('click', function() {
+    if ($('.header .nav').attr('data-nav-status') !== 'open') {
+      $('.header .nav-toggle i.fa').removeClass('fa-bars').addClass('fa-times');
+      $('.header .nav').slideDown(200, function() {
+        $(this).attr('data-nav-status', 'open').removeAttr('style');
+      });
+    } else {
+      $('.header .nav-toggle i.fa').removeClass('fa-times').addClass('fa-bars');
+      $('.header .nav').slideUp(200, function() {
+        $(this).attr('data-nav-status', 'closed').removeAttr('style');
+      });
+    }
+  });
+
+
+  // Home page hero slideshow
+  // -------------------------
+
   $('.hero').removeClass('js-hide').slick({
     autoplay: true,
     autoplaySpeed: 6000,
