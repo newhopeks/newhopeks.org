8f072134c317ab778e9cea8d86fba039bdb71792
Nate Dillon <natedillon@gmail.com>
2017-08-02 15:41:39 -0500

    Removed unused slideshow script

--- sites/all/themes/newhopeks/js/script.js
+++ sites/all/themes/newhopeks/js/script.js
@@ -13,49 +13,6 @@ Copyright:  (c) 2014 New Hope Church
 
 (function ($) { $(document).ready(function() {
 
-  /*
-   * Slideshow
-   * -------------------------------------------------------------------------------------------------
-   */
-
-  /*
-  // turn the slideshow on or off
-  var showSlideshow = false;
-
-  // choose the slideshow script to use ('slides' or 'nivoSlider')
-  var slideshowScript = 'slides';
-
-  if (showSlideshow == true) {
-
-    if (typeof slideshowItems !== 'undefined') {
-
-      // get the images source code
-      var items = slideshowItems;
-
-      // clear the div and add the images
-      $('.slideshow .slides-container').empty().append(items);
-
-    }
-
-    // start the slideshow
-    if (slideshowScript === 'slides') {
-      $('.slideshow').slides({
-        container: 'slides-container',
-        pagination: false,
-        generatePagination: false,
-        effect: 'fade',
-        fadeSpeed: 2000,
-        crossfade: true,
-        play: 7000
-      });
-    }
-    if (slideshowScript === 'nivoSlider') {
-      $('.slideshow').nivoSlider();
-    }
-
-  }
-  */
-
   $('.hero').removeClass('js-hide').slick({
     autoplay: true,
     autoplaySpeed: 6000,
